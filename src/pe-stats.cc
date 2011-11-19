5d24bef7a1baf67755b50bc798403eb3d3a615c6
Riccardo Vicedomini <ric.vice@gmail.com>
2011-11-19 19:36:38 +0100

    Added executable for calculating some statistics about paired-end reads mapping.

---
+++ src/pe-stats.cc
@@ -0,0 +1,79 @@
+/* 
+ * File:   pe-stats.cc
+ * Author: riccardo
+ *
+ * Created on 1 agosto 2011, 22.39
+ */
+
+#include <cstdlib>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <list>
+#include <algorithm>
+
+#include "api/BamReader.h"
+
+using namespace BamTools;
+
+std::string getPathBaseName( std::string path )
+{
+    size_t found = path.rfind ( '/' );
+    return (found != std::string::npos) ? path.substr(found) : std::string( path.c_str() );
+}
+
+int main(int argc, char** argv) 
+{
+    if( argc != 3 )
+    {
+        std::cout << "Usage " << getPathBaseName( std::string(argv[0]) ) << "<insert length> <alignment.bam>" << std::endl;
+        return 1;
+    }
+    
+    std::string bamFile = argv[2];
+    int32_t insertLength = atoi(argv[1]);
+    
+    int32_t minTemplateLen = insertLength - (insertLength/2);
+    int32_t maxTemplateLen = insertLength + (insertLength/2);
+    
+    BamReader inBam;
+    inBam.Open( bamFile );
+    
+    BamAlignment align;
+    uint32_t reads = 0, mapped = 0, uniMapped = 0, peMapped = 0, peCorrectlyMapped = 0, peCorrectlyMapped2 = 0;
+    char type;
+        
+    while( inBam.GetNextAlignment(align) )
+    {
+        reads++;
+        
+        if( align.IsMapped() ) mapped++;
+        
+        // uniquely mapped reads
+        if( align.IsMapped() && align.GetTagType( std::string("XT"), type ) )
+        {
+            if( type == 'U' ) uniMapped++;
+        }
+        
+        if( align.Length >= minTemplateLen && align.Length <= maxTemplateLen ) peCorrectlyMapped++;
+        
+        if( align.IsMapped() && align.IsMateMapped() )
+        {
+            if( align.IsPaired() && align.IsProperPair() && align.Length >= minTemplateLen && align.Length <= maxTemplateLen ) peCorrectlyMapped2++;
+        }
+    }
+    
+    inBam.Close();
+    
+    std::cout << "Reads: " << reads << "\n"
+              << "Mapped: " << mapped << "\n"
+              << "Uniquely Mapped: " << uniMapped << "\n"
+              << "Correctly Mapped: " << peCorrectlyMapped << "\n"
+              << "Correctly Mapped: " << peCorrectlyMapped2 << "\n"
+              << std::endl;
+    
+    return 0;
+}
+
